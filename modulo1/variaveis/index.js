d1479aecdd4d7ce1bec91fd32e6b3a106e43b3e2
Ivanilton Aparecido Dias <ivanilton.dias@gmail.com>
2022-02-08 15:38:37 -0300

    fis os exercicios dinovo por que mandei errado

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,177 @@
+/*
+
+aqui começa console.log
+
+*/
+console.log("hello word")
+
+const nome = prompt("qual é seu nome?")
+console.log(nome)
+
+
+/*Exercício 1 
+
+let a = 10
+let b = 10
+
+console.log(b)//Aqui ira imprimir o valor de b que é 10, assim(10)
+
+b = 5
+console.log(a, b)//Aqui ira imprimir o valor de a que é 10 e o novo valor de b que é 5, assim(10,5)
+
+
+*/
+
+
+
+/* Exercício 2 
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)
+
+let a = 10
+let b = 20
+c = a
+b = c
+a = b
+
+console.log(a, b, c)//Aqui todos vaum imprimir o valor 10, assim (10 10 10)
+
+// O valor de b é 20 mais a esta recebendo b que recebeu c e c esta recebendo a que é igual a 10
+
+
+*/
+
+
+/*Exercício 3
+
+let horasTrabalhadas = prompt("Quantas horas você trabalha por dia?")
+let pagamentoDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${t/p} por hora`)
+
+//este codigo esta tirando a porcentagem trabalhadas por dia 10%
+
+
+*/ 
+
+
+//Exercicio 1 exercicio codigo)
+
+/*
+const nome
+let idade
+
+console.log (typeof nome)
+console.log(typeof idade)
+
+//Aqui deu um erro por que eu nao inicializei a variavel
+
+*/
+
+/*
+
+const nome = prompt("Qual seu nome?")
+let idade = prompt("Qual sua idade?")
+
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+console.log("Ola",nome, "Você tem", idade, "anos")
+
+/Os dois vaum imprimir string porque o prompt só recebe uma string
+
+
+*/ 
+
+
+
+
+/*
+
+const camisa = "Sua camisa é azul?"
+const calca = "Sua calça é tamanho 42?"
+const tenis = "Seu tenis é 41?"
+
+let falso = false
+let verdadeiro = true
+
+console.log("Sua camisa é azul?", verdadeiro)
+console.log("Sua calça é tamanho 42?", falso)
+console.log("Seu tenis é 41", verdadeiro)
+
+
+*/ 
+
+/*
+
+
+let a = 10
+let b = 20
+let c = null
+
+c = a
+a = b
+b = c
+
+
+console.log("O novo valor de a é", a)
+console.log("O novo valor de b é", b)
+
+*/
+
+
+
+/*
+
+const numero1 = prompt("Digite o primeiro numero")
+const numero2 = prompt("Digite o segundo numero")
+
+let n1 = Number(numero1)
+let n2 = Number(numero2)
+
+
+let resultSoma = n1+n2
+let resultMult = n1*n2
+
+console.log("O primeiro número somado ao segundo número resulta em", resultSoma)
+console.log("O primeiro número multiplicado ao segundo número resulta em", resultMult)
+
+
+
+*/
+
+
+
+
+/*
+
+
+Exercicio extra par ou impar
+
+const numero1 = prompt("Digite um numero")
+
+let x = Number(numero1)
+
+let resultado = x%2
+
+if(resultado != 0){
+
+    console.log("O numero é impar")
+
+}else{
+
+    console.log("O numero é par")
+}
+
+
+
+*/
+
+
+
