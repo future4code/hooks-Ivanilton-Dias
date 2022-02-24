3700bd24f3a54258f30628c07140e7b3e5bd688e
Ivanilton Aparecido Dias <ivanilton.dias@gmail.com>
2022-02-24 20:19:11 -0300

    Enterga exercicios condicionais 23/02/2022

---
+++ modulo1/condicionais/index.js
@@ -0,0 +1,114 @@
+//Exercícios de interpretação de código
+
+//1)
+//a
+//Irá verificar se o numero dividido por 2 
+//recebe o resto igual a 0 se for passa no teste
+//caso contrario nao passa
+
+//b para todos os numeros pares
+
+//c para todos os numeros impares
+
+//2
+//a
+//para verificar os valores das frutas
+
+//b
+//2.25
+
+//c
+//5.5 5
+
+//3
+//a
+//recebendo um const numero e 
+//mudando para number pelo prompt
+
+//b
+//se for o numero 10 a mensagem sera passou no teste 
+//Essa mensagem é secreta
+//-10 nao ira imprimir nada
+
+//c
+
+//xercícios de escrita de código
+//1
+
+//a b e c
+/*
+const idade = Number(prompt("Qual sua idade?"))
+
+if(idade>18){
+
+    console.log("Você pode dirigir")
+}else{
+    console.log("Você não pode dirigir")
+}
+*/
+/*
+
+//2
+const turno = prompt("Qual Horario você estuda?matutino(m), vespertino(v) e noturno(n)")
+
+if(turno.toUpperCase()=="M"){
+
+    console.log("Bom dia!")
+
+}else if(turno.toUpperCase()=="V"){
+
+    console.log("Boa tarde!!")
+
+}else{
+    console.log("Bom noite!")
+}
+*/
+
+//3
+
+/*
+const turno = prompt("Qual Horario você estuda?matutino(m), vespertino(v) e noturno(n)")
+
+switch (turno) {
+    case "m":
+
+        console.log("Bom dia!")
+
+        break;
+
+    case "v":
+
+        console.log("Boa tarde!")
+
+        break;
+
+    case "n":
+
+        console.log("Boa noite!")
+
+        break;
+
+
+    default:
+
+        console.log("ok!")
+
+        break;
+}
+
+*/
+
+//4
+/*
+const genero = prompt("Qual o genero do filme?")
+const valor = Number(prompt("Qual o valor do ingresso?"))
+
+if(genero != "fantasia" || valor > 15){
+
+    console.log("Escolha outro filme!")
+
+}else{
+
+    console.log("Bom filme!")
+}
+*/
